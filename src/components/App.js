cf8b0c794a50368c375da5afb9d5c9109d4e274d
George Bronner <george.bronner@gmail.com>
2024-06-30 20:14:24 -0400

    auth almost there

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import Header from "./Header";
-import Footer from "./Footer";
 import { RouterProvider } from 'react-router-dom';
 import router from "../Router";
 
 function App() {
+
+
     return (
         <div className="App">
-            <Header />
-            <RouterProvider router={router} />
-            <Footer />
+            <RouterProvider router={router}>
+            </RouterProvider>
         </div>
     );
 }
